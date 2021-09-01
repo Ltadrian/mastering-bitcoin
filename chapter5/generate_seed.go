cff2354ae9782731b9d2dff292489503de712127
Adrian Gracia <adriangracia@Adrians-MacBook-Pro-2.local>
2021-09-01 16:23:26 -0500

    generate master keys

--- chapter5/generate_seed.go
+++ chapter5/generate_seed.go
@@ -1,7 +1,9 @@
 package chapter5
 
 import (
+	"crypto/hmac"
 	"crypto/sha512"
+	"encoding/hex"
 	"strings"
 
 	"golang.org/x/crypto/pbkdf2"
@@ -19,4 +21,18 @@ func GenerateSeed(wordList []string, salt string) []byte {
 		64,
 		sha512.New,
 	)
+}
+
+func GenerateMasterKeys(seed []byte) (masterPrivateKey, masterChainCode string) {
+	sha512hmac := hmac.New(sha512.New, []byte{})
+	_, err := sha512hmac.Write(seed)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get result and encode as hexadecimal string
+	sha512hmac_b := sha512hmac.Sum(nil)
+	masterPrivateKey = hex.EncodeToString(sha512hmac_b[:32]) // left 256 bits
+	masterChainCode = hex.EncodeToString(sha512hmac_b[32:])  // right 256 bits
+	return masterPrivateKey, masterChainCode
 }
--- chapter5/generate_seed_test.go
+++ chapter5/generate_seed_test.go
@@ -13,3 +13,11 @@ func TestGenerateSeed(t *testing.T) {
 	fmt.Printf("master seed:%s\n len:%d\n", hex.EncodeToString(masterSeed), len(masterSeed))
 	assert.Equal(t, 64, len(masterSeed)) // 512 bits === 64 bytes
 }
+
+func TestGenerateMasterKeys(t *testing.T) {
+	masterPrivateKey, masterChainCode := GenerateMasterKeys(GenerateSeed(English[:12], "mnemonic"))
+	fmt.Printf("masterPrivateKey:%s\nlen:%d\n", masterPrivateKey, len(masterPrivateKey))
+	fmt.Printf("masterChainCode:%s\nlen:%d\n", masterChainCode, len(masterChainCode))
+	assert.Equal(t, 64, len(masterPrivateKey))
+	assert.Equal(t, 64, len(masterChainCode))
+}
